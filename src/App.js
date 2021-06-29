5ce7156866a1e03a229f02a704110fc35710bab5
Heggy Castaneda <heggyy@gmail.com>
2021-06-28 23:27:49 -0400

    count fx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from "react";
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const countUp = () => {
+    setCounter(counter + 1);
+  };
+
+  const countDown = () => {
+    setCounter(counter - 1);
+  };
+
+  const reset = () => {
+    setCounter(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>count at {counter}</h1>
+      <button onClick={countUp}>Up</button><br/><br/>
+      <button onClick={countDown}>Down</button><br/><br/>
+      <button onClick={reset}>Reset</button><br/><br/>
     </div>
   );
 }
